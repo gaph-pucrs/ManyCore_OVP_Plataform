c75bc81875598d63dc4a98502b0b64bcb36910da
iacanaw <iacanaw@gmail.com>
2019-07-22 23:26:03 -0300

    it's alive!

--- noc/module/module.op.tcl
+++ noc/module/module.op.tcl
@@ -10,7 +10,6 @@ ihwaddbus -instancename cpu6Bus -addresswidth 32
 ihwaddbus -instancename cpu7Bus -addresswidth 32
 ihwaddbus -instancename cpu8Bus -addresswidth 32
 
-
 ihwaddnet -instancename int0
 ihwaddnet -instancename int1
 ihwaddnet -instancename int2
@@ -21,50 +20,49 @@ ihwaddnet -instancename int6
 ihwaddnet -instancename int7
 ihwaddnet -instancename int8
 
-
-ihwaddprocessor -instancename cpu0 \ 
-                -vendor ovpworld.org -library processor -type or1k -version 1.0 \ 
-                -variant generic \ 
+ihwaddprocessor -instancename cpu0 \
+                -vendor ovpworld.org -library processor -type or1k -version 1.0 \
+                -variant generic \
                 -semihostname or1kNewlib
 
-ihwaddprocessor -instancename cpu1 \ 
-                -vendor ovpworld.org -library processor -type or1k -version 1.0 \ 
-                -variant generic \ 
+ihwaddprocessor -instancename cpu1 \
+                -vendor ovpworld.org -library processor -type or1k -version 1.0 \
+                -variant generic \
                 -semihostname or1kNewlib
 
-ihwaddprocessor -instancename cpu2 \ 
-                -vendor ovpworld.org -library processor -type or1k -version 1.0 \ 
-                -variant generic \ 
+ihwaddprocessor -instancename cpu2 \
+                -vendor ovpworld.org -library processor -type or1k -version 1.0 \
+                -variant generic \
                 -semihostname or1kNewlib
 
-ihwaddprocessor -instancename cpu3 \ 
-                -vendor ovpworld.org -library processor -type or1k -version 1.0 \ 
-                -variant generic \ 
+ihwaddprocessor -instancename cpu3 \
+                -vendor ovpworld.org -library processor -type or1k -version 1.0 \
+                -variant generic \
                 -semihostname or1kNewlib
 
-ihwaddprocessor -instancename cpu4 \ 
-                -vendor ovpworld.org -library processor -type or1k -version 1.0 \ 
-                -variant generic \ 
+ihwaddprocessor -instancename cpu4 \
+                -vendor ovpworld.org -library processor -type or1k -version 1.0 \
+                -variant generic \
                 -semihostname or1kNewlib
 
-ihwaddprocessor -instancename cpu5 \ 
-                -vendor ovpworld.org -library processor -type or1k -version 1.0 \ 
-                -variant generic \ 
+ihwaddprocessor -instancename cpu5 \
+                -vendor ovpworld.org -library processor -type or1k -version 1.0 \
+                -variant generic \
                 -semihostname or1kNewlib
 
-ihwaddprocessor -instancename cpu6 \ 
-                -vendor ovpworld.org -library processor -type or1k -version 1.0 \ 
-                -variant generic \ 
+ihwaddprocessor -instancename cpu6 \
+                -vendor ovpworld.org -library processor -type or1k -version 1.0 \
+                -variant generic \
                 -semihostname or1kNewlib
 
-ihwaddprocessor -instancename cpu7 \ 
-                -vendor ovpworld.org -library processor -type or1k -version 1.0 \ 
-                -variant generic \ 
+ihwaddprocessor -instancename cpu7 \
+                -vendor ovpworld.org -library processor -type or1k -version 1.0 \
+                -variant generic \
                 -semihostname or1kNewlib
 
-ihwaddprocessor -instancename cpu8 \ 
-                -vendor ovpworld.org -library processor -type or1k -version 1.0 \ 
-                -variant generic \ 
+ihwaddprocessor -instancename cpu8 \
+                -vendor ovpworld.org -library processor -type or1k -version 1.0 \
+                -variant generic \
                 -semihostname or1kNewlib
 
 ihwconnect -bus cpu0Bus -instancename cpu0 -busmasterport INSTRUCTION
@@ -176,7 +174,6 @@ ihwaddperipheral -instancename router6 -modelfile peripheral/pse.pse
 ihwaddperipheral -instancename router7 -modelfile peripheral/pse.pse
 ihwaddperipheral -instancename router8 -modelfile peripheral/pse.pse
 
-
 ihwconnect -instancename router0 -busslaveport localPort -bus cpu0Bus -loaddress 0x80000000 -hiaddress 0x8000000F
 ihwconnect -instancename router1 -busslaveport localPort -bus cpu1Bus -loaddress 0x80000000 -hiaddress 0x8000000F
 ihwconnect -instancename router2 -busslaveport localPort -bus cpu2Bus -loaddress 0x80000000 -hiaddress 0x8000000F
@@ -186,7 +183,6 @@ ihwconnect -instancename router5 -busslaveport localPort -bus cpu5Bus -loaddress
 ihwconnect -instancename router6 -busslaveport localPort -bus cpu6Bus -loaddress 0x80000000 -hiaddress 0x8000000F
 ihwconnect -instancename router7 -busslaveport localPort -bus cpu7Bus -loaddress 0x80000000 -hiaddress 0x8000000F
 ihwconnect -instancename router8 -busslaveport localPort -bus cpu8Bus -loaddress 0x80000000 -hiaddress 0x8000000F
-
 
 ihwaddpacketnet -instancename p_0_0_E
 ihwaddpacketnet -instancename p_0_0_W
@@ -208,7 +204,6 @@ ihwaddpacketnet -instancename p_2_2_E
 ihwaddpacketnet -instancename p_2_2_W
 ihwaddpacketnet -instancename p_2_2_N
 ihwaddpacketnet -instancename p_2_2_S
-
 
 ihwconnect -instancename router0 -packetnetport portEast -packetnet p_0_0_E
 ihwconnect -instancename router1 -packetnetport portWest -packetnet p_0_0_E
@@ -234,7 +229,6 @@ ihwconnect -instancename router8 -packetnetport portWest -packetnet p_2_2_W
 ihwconnect -instancename router7 -packetnetport portEast -packetnet p_2_2_W
 ihwconnect -instancename router8 -packetnetport portSouth -packetnet p_2_2_S
 ihwconnect -instancename router5 -packetnetport portNorth -packetnet p_2_2_S
-
 
 ihwconnect -instancename router0 -netport       INTTC  -net int0
 ihwconnect -instancename router1 -netport       INTTC  -net int1
