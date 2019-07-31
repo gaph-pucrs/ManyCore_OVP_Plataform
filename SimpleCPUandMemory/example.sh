d43d27f88554229e8fdd7942ba78a4ca185c072c
geaninne <geaninne.mnl@gmail.com>
2019-07-31 18:02:31 -0300

    adding examples

---
+++ SimpleCPUandMemory/example.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Check Installation supports this example
+#checkinstall.exe -p install.pkg --nobanner || exit
+
+CROSS=OR1K
+
+make -C application CROSS=${CROSS}
+
+make -C module
+
+#make -C peripheral NOVLNV=1
+
+# run the platform
+harness.exe \
+    --modulefile module/model.${IMPERAS_SHRSUF} \
+    --program cpu1=application/application.${CROSS}.elf $* \
+    --trace --verbose
+ 
+
