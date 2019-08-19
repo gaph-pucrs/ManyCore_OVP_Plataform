68cf4a5b5f62fbc3b5901fa2eff176af53371a23
iacanaw <iacanaw@gmail.com>
2019-08-19 14:50:55 -0300

    whnoc folder

---
+++ whnoc/application/applicationGenerator.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+X=$1
+Y=$2
+
+N=$(($X*$Y))
+N=$(($N-1))
+
+for i in $(seq 0 $N);
+do
+
+    cp -u -v source/applicationGeneric/applicationGeneric.c application$i.c
+
+done
+
+
+for i in $(seq 0 $N);
+do
+
+    	sed -i 's/LOG(\"Starting.*/LOG(\"Starting ROUTER'$i' application! \\n\\n\")\;/' application$i.c
+        sed -i 's/LOG(\"Application.*/LOG(\"Application ROUTER'$i' done!\\n\\n\")\;/' application$i.c               
+done
+
+cont=0
+for i in $(seq 0 $(($Y-1)));
+do
+
+    for j in $(seq 0 $(($X-1)));
+    do
+        sed -i 's/\*myAddress = 0x.*/\*myAddress = 0x'$j$i'\;/' application$cont.c 
+        sed -i 's/\*PEToSync = 0x.*/\*PEToSync = 0x'$j$i'\;/' application$cont.c 
+        cont=$((cont+1))
+    done
+done
+
+
