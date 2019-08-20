ae1cf6eb1e22ec1a1406ef252779b070215ec913
geaninne <geaninne.mnl@gmail.com>
2019-08-20 10:56:39 -0300

    "script alterado"

--- whnoc/peripheral/whnoc/ovp_compiler.sh
+++ whnoc/peripheral/whnoc/ovp_compiler.sh
@@ -16,9 +16,4 @@ harness/harness.$IMPERAS_ARCH.exe \
      --program cpu1=application/application1.${CROSS}.elf $* \
      --program cpu2=application/application2.${CROSS}.elf $* \
      --program cpu3=application/application3.${CROSS}.elf $* \
-     --program cpu4=application/application4.${CROSS}.elf $* \
-     --program cpu5=application/application5.${CROSS}.elf $* \
-     --program cpu6=application/application6.${CROSS}.elf $* \
-     --program cpu7=application/application7.${CROSS}.elf $* \
-     --program cpu8=application/application8.${CROSS}.elf $* \
 --verbose 
