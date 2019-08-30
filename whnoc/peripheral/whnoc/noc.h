48391f5c2e300e002c37db5b243eddbbb5f8f1f0
Iaçanã Ianiski Weber <iacanaw@gmail.com>
2019-08-30 03:24:16 -0300

    ticked WHNOC

--- whnoc/peripheral/whnoc/noc.h
+++ whnoc/peripheral/whnoc/noc.h
@@ -5,6 +5,7 @@
 #define SOUTH 	3
 #define LOCAL 	4
 #define ND      5 // Not Defined
+#define DMNI    6
 
 // NoC Structure configuration
 #define N_PORTS 	    5
@@ -21,5 +22,7 @@
 #define EMPTY   0
 #define HEADER  0xFFFFFFFF
 #define SIZE    0xFFFFFFFD
+
+#define DMNI_SIZE 25600
 
 #define N_PES 25
