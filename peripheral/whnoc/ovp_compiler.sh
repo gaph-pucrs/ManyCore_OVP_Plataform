541ff490f04e5ff79c96c2b395bb5fc481c97071
iacanaw <iacanaw@gmail.com>
2019-09-06 16:09:36 -0300

    a new branch

--- peripheral/whnoc/ovp_compiler.sh
+++ peripheral/whnoc/ovp_compiler.sh
@@ -9,9 +9,11 @@ make -C application CROSS=${CROSS}
 make -C module
 make -C peripheral/whnoc NOVLNV=1
 make -C peripheral/synchronizer NOVLNV=1
-harness.exe --modulefile module/model.${IMPERAS_SHRSUF} \
+make -C harness
+harness.exe --modulefile module/model.${IMPERAS_SHRSUF}
+harness/harness.$IMPERAS_ARCH.exe \
      --program cpu0=application/application0.${CROSS}.elf $* \
      --program cpu1=application/application1.${CROSS}.elf $* \
      --program cpu2=application/application2.${CROSS}.elf $* \
      --program cpu3=application/application3.${CROSS}.elf $* \
-     --verbose 
+--verbose 
---
+++ run.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+X=$1
+Y=$2
+N=$(($X*$Y))
+
+source /soft64/source_gaph
+module load ovp/20170201
+source /soft64/imperas/ferramentas/64bits/Imperas.20170201/bin/setup.sh
+setupImperas /soft64/imperas/ferramentas/64bits/Imperas.20170201
+
+cd application
+./applicationGenerator.sh $X $Y
+cd ..
+
+cd module
+./moduleGenerator.sh $X $Y
+cd ..
+
+cd peripheral
+cd whnoc
+sed -i 's/#define N_PE.*/#define N_PES '$N'/' noc.h
+cd ../..
+
+N=$(($N-1))
+
+rm -rf ovp_compiler.sh
+echo "#!/bin/sh" >> ovp_compiler.sh
+echo "cd peripheral" >> ovp_compiler.sh
+echo "rm -rf pse.pse" >> ovp_compiler.sh
+echo "cd .." >> ovp_compiler.sh
+echo "# Check Installation supports this example" >> ovp_compiler.sh
+echo "checkinstall.exe -p install.pkg --nobanner || exit" >> ovp_compiler.sh
+echo "CROSS=OR1K" >> ovp_compiler.sh
+echo "make -C application CROSS=\${CROSS}" >> ovp_compiler.sh
+echo "make -C module" >> ovp_compiler.sh
+echo "make -C peripheral/whnoc NOVLNV=1" >> ovp_compiler.sh
+echo "make -C peripheral/synchronizer NOVLNV=1" >> ovp_compiler.sh
+#harness/harness.$IMPERAS_ARCH.exe --program application/application.OR1K.elf 
+echo "harness.exe --modulefile module/model.\${IMPERAS_SHRSUF} \\" >> ovp_compiler.sh
+
+#echo " --modulefile module/model.\${IMPERAS_SHRSUF} \\" >> ovp_compiler.sh
+
+for i in $(seq 0 $N);
+do
+    if [ $i != $N ];
+    then
+        echo "     --program cpu"$i"=application/application"$i".\${CROSS}.elf \$* \\" >> ovp_compiler.sh
+    else
+        echo "     --program cpu"$i"=application/application"$i".\${CROSS}.elf \$* \\" >> ovp_compiler.sh
+    fi
+done
+
+        echo "     --verbose " >> ovp_compiler.sh
+
+chmod +x ovp_compiler.sh
+
+./ovp_compiler.sh
