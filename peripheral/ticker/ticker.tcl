d02b8e16b4f2317919899f85a9bd775088ddcfe9
iacanaw <iacanaw@gmail.com>
2019-09-15 01:55:08 -0300

    script bug fixed

--- peripheral/ticker/ticker.tcl
+++ peripheral/ticker/ticker.tcl
@@ -1,10 +1,9 @@
-
 imodelnewperipheral -name ticker \
                     -constructor constructor \
                     -destructor  destructor \
                     -vendor gaph \
                     -library peripheral \
-                    -version 1.0 
+                    -version 1.0
 
 iadddocumentation -name Description \
                   -text "A router ticker"
@@ -14,151 +13,152 @@ iadddocumentation -name Description \
 #########################################
 
 imodeladdpacketnetport  \
-    -name tickPort0 \
-    -maxbytes 4 \
-    -updatefunction tick0 \
-    -updatefunctionargument 0x00
+      -name tickPort0 \
+      -maxbytes 4 \
+      -updatefunction tick0 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort1 \
-    -maxbytes 4 \
-    -updatefunction tick1 \
-    -updatefunctionargument 0x00
+      -name tickPort1 \
+      -maxbytes 4 \
+      -updatefunction tick1 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort2 \
-    -maxbytes 4 \
-    -updatefunction tick2 \
-    -updatefunctionargument 0x00
+      -name tickPort2 \
+      -maxbytes 4 \
+      -updatefunction tick2 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort3 \
-    -maxbytes 4 \
-    -updatefunction tick3 \
-    -updatefunctionargument 0x00
+      -name tickPort3 \
+      -maxbytes 4 \
+      -updatefunction tick3 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort4 \
-    -maxbytes 4 \
-    -updatefunction tick4 \
-    -updatefunctionargument 0x00
+      -name tickPort4 \
+      -maxbytes 4 \
+      -updatefunction tick4 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort5 \
-    -maxbytes 4 \
-    -updatefunction tick5 \
-    -updatefunctionargument 0x00
+      -name tickPort5 \
+      -maxbytes 4 \
+      -updatefunction tick5 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort6 \
-    -maxbytes 4 \
-    -updatefunction tick6 \
-    -updatefunctionargument 0x00
+      -name tickPort6 \
+      -maxbytes 4 \
+      -updatefunction tick6 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort7 \
-    -maxbytes 4 \
-    -updatefunction tick7 \
-    -updatefunctionargument 0x00
+      -name tickPort7 \
+      -maxbytes 4 \
+      -updatefunction tick7 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort8 \
-    -maxbytes 4 \
-    -updatefunction tick8 \
-    -updatefunctionargument 0x00
+      -name tickPort8 \
+      -maxbytes 4 \
+      -updatefunction tick8 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort9 \
-    -maxbytes 4 \
-    -updatefunction tick9 \
-    -updatefunctionargument 0x00
+      -name tickPort9 \
+      -maxbytes 4 \
+      -updatefunction tick9 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort10 \
-    -maxbytes 4 \
-    -updatefunction tick10 \
-    -updatefunctionargument 0x00
+      -name tickPort10 \
+      -maxbytes 4 \
+      -updatefunction tick10 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort11 \
-    -maxbytes 4 \
-    -updatefunction tick11 \
-    -updatefunctionargument 0x00
+      -name tickPort11 \
+      -maxbytes 4 \
+      -updatefunction tick11 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort12 \
-    -maxbytes 4 \
-    -updatefunction tick12 \
-    -updatefunctionargument 0x00
+      -name tickPort12 \
+      -maxbytes 4 \
+      -updatefunction tick12 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort13 \
-    -maxbytes 4 \
-    -updatefunction tick13 \
-    -updatefunctionargument 0x00
+      -name tickPort13 \
+      -maxbytes 4 \
+      -updatefunction tick13 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort14 \
-    -maxbytes 4 \
-    -updatefunction tick14 \
-    -updatefunctionargument 0x00
+      -name tickPort14 \
+      -maxbytes 4 \
+      -updatefunction tick14 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort15 \
-    -maxbytes 4 \
-    -updatefunction tick15 \
-    -updatefunctionargument 0x00
+      -name tickPort15 \
+      -maxbytes 4 \
+      -updatefunction tick15 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort16 \
-    -maxbytes 4 \
-    -updatefunction tick16 \
-    -updatefunctionargument 0x00
+      -name tickPort16 \
+      -maxbytes 4 \
+      -updatefunction tick16 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort17 \
-    -maxbytes 4 \
-    -updatefunction tick17 \
-    -updatefunctionargument 0x00
+      -name tickPort17 \
+      -maxbytes 4 \
+      -updatefunction tick17 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort18 \
-    -maxbytes 4 \
-    -updatefunction tick18 \
-    -updatefunctionargument 0x00
+      -name tickPort18 \
+      -maxbytes 4 \
+      -updatefunction tick18 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort19 \
-    -maxbytes 4 \
-    -updatefunction tick19 \
-    -updatefunctionargument 0x00
+      -name tickPort19 \
+      -maxbytes 4 \
+      -updatefunction tick19 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort20 \
-    -maxbytes 4 \
-    -updatefunction tick20 \
-    -updatefunctionargument 0x00
+      -name tickPort20 \
+      -maxbytes 4 \
+      -updatefunction tick20 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort21 \
-    -maxbytes 4 \
-    -updatefunction tick21 \
-    -updatefunctionargument 0x00
+      -name tickPort21 \
+      -maxbytes 4 \
+      -updatefunction tick21 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort22 \
-    -maxbytes 4 \
-    -updatefunction tick22 \
-    -updatefunctionargument 0x00
+      -name tickPort22 \
+      -maxbytes 4 \
+      -updatefunction tick22 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort23 \
-    -maxbytes 4 \
-    -updatefunction tick23 \
-    -updatefunctionargument 0x00
+      -name tickPort23 \
+      -maxbytes 4 \
+      -updatefunction tick23 \
+      -updatefunctionargument 0x00
 
 imodeladdpacketnetport  \
-    -name tickPort24 \
-    -maxbytes 4 \
-    -updatefunction tick24 \
-    -updatefunctionargument 0x00
+      -name tickPort24 \
+      -maxbytes 4 \
+      -updatefunction tick24 \
+      -updatefunctionargument 0x00
+
