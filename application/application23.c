31f51ec06ed34ae55200633bd081fb68c8d24200
iacanaw <iacanaw@gmail.com>
2020-01-14 15:31:59 -0300

    transpose6x6 name fixed

---
+++ application/application23.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "interrupt.h"
+#include "spr_defs.h"
+#include "source/API/api.h"
+
+#include "transpose6x6_config.h"
+
+message theMessage;
+
+int main(int argc, char **argv)
+{
+    OVP_init();
+    //////////////////////////////////////////////////////
+    /////////////// YOUR CODE START HERE /////////////////
+    //////////////////////////////////////////////////////
+    int i;
+    theMessage.size = MESSAGE_SIZE;
+    for(i=0;i<theMessage.size;i++){
+        theMessage.msg[i] = (i+1)*2;
+    }
+    for(i=0;i<N_MESSAGES;i++){
+        SendMessage(&theMessage, transpose12_addr);
+        ReceiveMessage(&theMessage, transpose12_addr);
+    }
+    
+    //////////////////////////////////////////////////////
+    //////////////// YOUR CODE ENDS HERE /////////////////
+    //////////////////////////////////////////////////////
+    finishApplication();
+    return 1;
+}
