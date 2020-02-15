cf8611ea1ddebebbb98b624a7244ee4ab5739d9c
iacanaw <iacanaw@gmail.com>
2020-02-15 15:38:39 -0300

    send function configuration

--- application/source/API/api.h
+++ application/source/API/api.h
@@ -65,7 +65,7 @@ void OVP_init();
 unsigned int bufferHasSpace();
 void SendMessage(message *theMessage, unsigned int destination);
 void SendRaw(unsigned int addr);
-void SendRaw_II(unsigned int addr, unsigned int slot);
+void SendSlot(unsigned int addr, unsigned int slot);
 unsigned int checkPendingReq(unsigned int destID);
 unsigned int getEmptyIndex();
 void bufferPush(unsigned int index);
@@ -226,7 +226,7 @@ unsigned int sendFromMsgBuffer(unsigned int requester){
         //if(*NIcmd == IDLE || *NIcmd == DONE){
         //int_disable(0);    
         if(*NIcmd == NI_STATUS_OFF){
-            SendRaw_II((unsigned int)&buffer_packets[found], found);
+            SendSlot((unsigned int)&buffer_packets[found], found);
             /**NIaddr = (unsigned int)&buffer_packets[found];
             *NIcmd = TX;*/
         }
@@ -246,7 +246,7 @@ unsigned int sendFromMsgBuffer(unsigned int requester){
                 LOG("%x - ERROR! Unexpected interruption! (NI_INT_TYPE_TX) - can not handle it! Call the SAC!\n",*myAddress);
                 while(1){}
             }
-            SendRaw_II((unsigned int)&buffer_packets[found], found);
+            SendSlot((unsigned int)&buffer_packets[found], found);
         }
         //transmittingActive = found;
         //int_enable(0);
@@ -290,7 +290,7 @@ void requestMsg(unsigned int from){
     myServicePacket[PI_SERVICE] = MESSAGE_REQ;
     myServicePacket[PI_REQUESTER] = *myAddress;
     //int_disable(0);
-    SendRaw_II((unsigned int)&myServicePacket, 0xFFFFFFFE); // WARNING: This may cause a problem!!!!
+    SendSlot((unsigned int)&myServicePacket, 0xFFFFFFFE); // WARNING: This may cause a problem!!!!
     //transmittingActive = 0xFFFFFFFE;         // Because the SendRaw could be interrupted during the execution (by the quantum end) and the transmittingActive is modified only after the execution.
     //int_enable(0);
     //LOG("=%d===========================REQ ENVIADO PRA NI!\n",getID(*myAddress));
@@ -323,7 +323,7 @@ void SendMessage(message *theMessage, unsigned int destination){
         //LOG("PENDING REQUEST ENCONTRADO!\n");
         // Sends the packet
         //int_disable(0);
-        SendRaw_II((unsigned int)&buffer_packets[index], index);
+        SendSlot((unsigned int)&buffer_packets[index], index);
         //transmittingActive = index;
         //int_enable(0);
         // Clear the pending request
@@ -375,18 +375,14 @@ unsigned int getID(unsigned int address){
 }
 ///////////////////////////////////////////////////////////////////
 // Configure the NI to transmitt a given packet
-void SendRaw_II(unsigned int addr, unsigned int slot){
+void SendSlot(unsigned int addr, unsigned int slot){
     while(*NIcmd != NI_STATUS_OFF){/*waits until NI is ready to execute an operation*/}
-    //while(*NIcmd!=IDLE && *NIcmd!=DONE){ /*LOG("eu: %x status:%x\n",*myAddress,*NIcmd);/*waits until NI is ready to execute an operation*/}
-    *NIaddr = addr;
     transmittingActive = slot;
-    *NIcmd = TX;
+    SendRaw(addr);
 }
 ///////////////////////////////////////////////////////////////////
 // Configure the NI to transmitt a given packet
 void SendRaw(unsigned int addr){
-    while(*NIcmd != NI_STATUS_OFF){/*waits until NI is ready to execute an operation*/}
-    //while(*NIcmd!=IDLE && *NIcmd!=DONE){ /*LOG("eu: %x status:%x\n",*myAddress,*NIcmd);/*waits until NI is ready to execute an operation*/}
     *NIaddr = addr;
     *NIcmd = TX;
 }
