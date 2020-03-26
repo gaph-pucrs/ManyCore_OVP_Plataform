7a6137bd2f37fc704b82c022f15b2018fec48bf2
Iaçanã Ianiski Weber <iacanaw@gmail.com>
2020-03-26 20:17:52 -0300

    Update tea.c

--- peripheral/tea/tea.c
+++ peripheral/tea/tea.c
@@ -10,13 +10,21 @@
 #include "tea.igen.h"
 //////////////////////////////// Callback stubs ////////////////////////////////
 
+#define __bswap_constant_32(x) \
+     ((((x) & 0xff000000) >> 24) | (((x) & 0x00ff0000) >>  8) |		      \
+      (((x) & 0x0000ff00) <<  8) | (((x) & 0x000000ff) << 24))
+
+unsigned int htonl(unsigned int x){
+    return __bswap_constant_32(x);
+}
+
 PPM_PACKETNET_CB(controlUpdate) {
     // YOUR CODE HERE (controlUpdate)
 }
 
 PPM_PACKETNET_CB(dataUpdate) {
     unsigned int newFlit = *(unsigned int *)data;
-    bhmMessage("I", "TEA", "Chegou um flit: %x", newFlit);
+    bhmMessage("I", "TEA", "Chegou um flit: %x", htonl(newFlit));
 }
 
 PPM_CONSTRUCTOR_CB(constructor) {
