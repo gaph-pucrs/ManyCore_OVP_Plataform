12f62c5b7eb95937978472a208b94c43d0ecef3c
iacanaw <iacanaw@gmail.com>
2020-03-30 15:55:03 -0300

    new iterator

---
+++ peripheral/iteratorMonoTrigger/iterator.tcl
@@ -0,0 +1,79 @@
+imodelnewperipheral -name iteratorMonoTrigger \
+                    -constructor constructor \
+                    -destructor  destructor \
+                    -vendor gaph \
+                    -library peripheral \
+                    -version 1.0
+
+iadddocumentation -name Description \
+                  -text "The NoC iteratorMonoTrigger"
+
+#############################################
+## Data ports between iterator and routers ##
+#############################################
+
+#########################################
+## A slave port on the processor bus
+#########################################
+imodeladdbusslaveport -name iteratorReg -size 4 -mustbeconnected
+
+# Address block for 8 bit control registers
+imodeladdaddressblock -name ab8 -port iteratorReg -offset 0x0 -width 32 -size 4
+
+# 8 bit control registers
+imodeladdmmregister -addressblock iteratorReg/ab8 -name iterationPort -readfunction iterateRead -writefunction iterateWrite -offset 0
+
+imodeladdpacketnetport  \
+      -name iterationPort0 \
+      -maxbytes 8 \
+      -updatefunction iteration0 \
+      -updatefunctionargument 0x00
+
+imodeladdpacketnetport  \
+      -name iterationPort1 \
+      -maxbytes 8 \
+      -updatefunction iteration1 \
+      -updatefunctionargument 0x00
+
+imodeladdpacketnetport  \
+      -name iterationPort2 \
+      -maxbytes 8 \
+      -updatefunction iteration2 \
+      -updatefunctionargument 0x00
+
+imodeladdpacketnetport  \
+      -name iterationPort3 \
+      -maxbytes 8 \
+      -updatefunction iteration3 \
+      -updatefunctionargument 0x00
+
+imodeladdpacketnetport  \
+      -name iterationPort4 \
+      -maxbytes 8 \
+      -updatefunction iteration4 \
+      -updatefunctionargument 0x00
+
+imodeladdpacketnetport  \
+      -name iterationPort5 \
+      -maxbytes 8 \
+      -updatefunction iteration5 \
+      -updatefunctionargument 0x00
+
+imodeladdpacketnetport  \
+      -name iterationPort6 \
+      -maxbytes 8 \
+      -updatefunction iteration6 \
+      -updatefunctionargument 0x00
+
+imodeladdpacketnetport  \
+      -name iterationPort7 \
+      -maxbytes 8 \
+      -updatefunction iteration7 \
+      -updatefunctionargument 0x00
+
+imodeladdpacketnetport  \
+      -name iterationPort8 \
+      -maxbytes 8 \
+      -updatefunction iteration8 \
+      -updatefunctionargument 0x00
+
---
+++ peripheral/iteratorMonoTrigger/iteratorMonoTrigger.igen.h
@@ -0,0 +1,77 @@
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//                W R I T T E N   B Y   I M P E R A S   I G E N
+//
+//                             Version 20170201.0
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ITERATORMONOTRIGGER_IGEN_H
+#define ITERATORMONOTRIGGER_IGEN_H
+
+#ifdef _PSE_
+#    include "peripheral/impTypes.h"
+#    include "peripheral/bhm.h"
+#    include "peripheral/bhmHttp.h"
+#    include "peripheral/ppm.h"
+#else
+#    include "hostapi/impTypes.h"
+#endif
+
+//////////////////////////////////// Externs ///////////////////////////////////
+
+extern Uns32 diagnosticLevel;
+
+
+/////////////////////////// Dynamic Diagnostic Macros //////////////////////////
+
+// Bottom two bits of word used for PSE diagnostics
+#define PSE_DIAG_LOW      (BHM_DIAG_MASK_LOW(diagnosticLevel))
+#define PSE_DIAG_MEDIUM   (BHM_DIAG_MASK_MEDIUM(diagnosticLevel))
+#define PSE_DIAG_HIGH     (BHM_DIAG_MASK_HIGH(diagnosticLevel))
+// Next two bits of word used for PSE semihost/intercept library diagnostics
+#define PSE_DIAG_SEMIHOST (BHM_DIAG_MASK_SEMIHOST(diagnosticLevel))
+
+/////////////////////////// Register data declaration //////////////////////////
+
+/////////////////////////////// Port Declarations //////////////////////////////
+
+#ifdef _PSE_
+///////////////////////////////// Port handles /////////////////////////////////
+
+typedef struct handlesS {
+    ppmPacketnetHandle    iterationPort0;
+    ppmPacketnetHandle    iterationPort1;
+    ppmPacketnetHandle    iterationPort2;
+    ppmPacketnetHandle    iterationPort3;
+    ppmPacketnetHandle    iterationPort4;
+    ppmPacketnetHandle    iterationPort5;
+    ppmPacketnetHandle    iterationPort6;
+    ppmPacketnetHandle    iterationPort7;
+    ppmPacketnetHandle    iterationPort8;
+} handlesT, *handlesTP;
+
+extern handlesT handles;
+
+////////////////////////////// Callback prototypes /////////////////////////////
+
+PPM_PACKETNET_CB(iteration0);
+PPM_PACKETNET_CB(iteration1);
+PPM_PACKETNET_CB(iteration2);
+PPM_PACKETNET_CB(iteration3);
+PPM_PACKETNET_CB(iteration4);
+PPM_PACKETNET_CB(iteration5);
+PPM_PACKETNET_CB(iteration6);
+PPM_PACKETNET_CB(iteration7);
+PPM_PACKETNET_CB(iteration8);
+PPM_CONSTRUCTOR_CB(periphConstructor);
+PPM_DESTRUCTOR_CB(periphDestructor);
+PPM_CONSTRUCTOR_CB(constructor);
+PPM_DESTRUCTOR_CB(destructor);
+PPM_SAVE_STATE_FN(peripheralSaveState);
+PPM_RESTORE_STATE_FN(peripheralRestoreState);
+
+#endif
+
+#endif
