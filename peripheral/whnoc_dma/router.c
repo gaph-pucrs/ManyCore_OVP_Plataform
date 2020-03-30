16b380aaac9f13fe90814248d6e1e2d227a9bc18
Iaçanã Ianiski Weber <iacanaw@gmail.com>
2020-03-30 20:54:11 -0300

    Update router.c

--- peripheral/whnoc_dma/router.c
+++ peripheral/whnoc_dma/router.c
@@ -229,6 +229,7 @@ void bufferStatusUpdate(unsigned int port){
 void bufferPush(unsigned int port){
     // Write a new flit in the buffer
     buffers[port][last[port]] = incomingFlit;
+    bhmMessage("I", "PUSH", "Flit value: %x", incomingFlit.data);
     if(last[port] < BUFFER_SIZE-1){
         last[port]++;
     }
