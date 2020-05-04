74ce85c08459addc2515405bf7aa9cdc53fa23f6
Geaninne <geaninne.mnl@gmail.com>
2020-05-04 16:41:00 -0300

    testando transmissao entre NoCs

--- peripheral/secNoC/secRouter.c
+++ peripheral/secNoC/secRouter.c
@@ -168,7 +168,7 @@ PPM_PACKETNET_CB(unsafeNoC) {
         incomingFlit.source = myID;
         incomingFlit.data = *(unsigned int *) data;
         incomingFlit.data = htonl1((unsigned int)incomingFlit.data);
-        bhmMessage("INFO", "SECROUTER", "-------------------------------------------------------------> flitTotal = %d received" incomingFlit.data);
+        bhmMessage("INFO", "SECROUTER", "-------------------------------------------------------------> flitTotal = %d received", incomingFlit.data);
         bufferPush(LOCAL);
 
     }
