0125d18dc693e9280ac96225d95a3b53f865f58e
Geaninne <geaninne.mnl@gmail.com>
2020-05-04 16:56:08 -0300

    testando bufferPush

--- peripheral/secNoC/secRouter.c
+++ peripheral/secNoC/secRouter.c
@@ -169,7 +169,7 @@ PPM_PACKETNET_CB(unsafeNoC) {
         incomingFlit.data = *(unsigned int *) data;
         //incomingFlit.data = htonl1(incomingFlit.data);
         bhmMessage("INFO", "SECROUTER", "-------------------------------------------------------------> flitTotal = %d received", incomingFlit.data);
-       // bufferPush(LOCAL);
+        bufferPush(LOCAL);
 
     }
 
