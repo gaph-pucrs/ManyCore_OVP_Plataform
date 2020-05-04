bd5c924c40fb35ade86c296ff7f522c46b54f39a
Iaçanã Ianiski Weber <iacanaw@gmail.com>
2020-05-04 05:03:05 -0300

    Update thermalMaster.c

--- application/source/thermalManagement/thermalMaster.c
+++ application/source/thermalManagement/thermalMaster.c
@@ -9,23 +9,48 @@
 
 message theMsg;
 
+static unsigned int energyLocalsDif_total[XCLUSTER][YCLUSTER];
+
+unsigned int Power[DIM_X*DIM_Y];
+unsigned int Temperature[DIM_X*DIM_Y];
+unsigned int energyLocalsDif_total[DIM_X][DIM_Y];
+
 int main(int argc, char **argv)
 {
     OVP_init();
     //////////////////////////////////////////////////////
     /////////////// YOUR CODE START HERE /////////////////
     //////////////////////////////////////////////////////
-    int i, j;
+    int y, x, p_idx = 0;;
     int ordem[DIM_X*DIM_Y];
+
+    /*Initialization*/
+    for(y=0;y<DIM_Y;y++){
+        for(x=0;x<DIM_X;x++){
+            Power[p_idx] = 0;
+            Temperature[p_idx] = 31815;
+            p_idx++;
+        }
+    }
+    /* Wait for every PE to send each power estimation */
     while(*SyncToPE != 1){ // Repete este processo enquanto houverem outras tarefas executando!
         // Aguarda os pacotes de energia dos PEs
-        for(i=0;i<DIM_X;i++){
-            for(j=0;j<DIM_Y;j++){
+        for(y=0;y<DIM_Y;y++){
+            for(x=0;x<DIM_X;x++){
                 ReceiveRaw(&theMsg);
+                energyLocalsDif_total[getXpos(theMsg.msg[3])][getYpos(theMsg.msg[3])] = theMsg.msg[1]; // total energy
+                p_idx++;
                 //LOG("%x - window: %u -- energy: %u -- leak: %u\n",theMsg.msg[3],theMsg.msg[0],theMsg.msg[1],theMsg.msg[2]);
             }
         }
         LOG("Todos os pacotes foram recebidos!!!\n");
+        for(y=0;y<DIM_Y;y++){
+            for(x=0;x<DIM_X;x++){
+                Power[p_idx] = (unsigned int)((energyLocalsDif_total[x][y])*64/1000/100)*128/100; // return energyLocalsDif_total[x][y]*64/1000/100;
+                LOG("%d,%d - energy: %u\n",x,y,Power[p_idx]);
+                p_idx++;
+            }
+        }
     }
     //////////////////////////////////////////////////////
     //////////////// YOUR CODE ENDS HERE /////////////////
