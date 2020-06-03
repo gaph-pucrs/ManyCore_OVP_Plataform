fd1dc3e0ca8e32252bc975228af5e0b21b1daa35
iacanaw <iacanaw@gmail.com>
2020-06-02 23:02:18 -0300

    Update api.h

--- application/source/API/api.h
+++ application/source/API/api.h
@@ -738,7 +738,7 @@ void OVP_init(){
 /* Verify if a message for a given requester is inside the buffer, if yes then send it and return 1 else returns 0 */
 unsigned int sendFromMsgBuffer(unsigned int requester){
     int i;
-    //LOG("~~~~> procurando pacote no pipe!! eu: %x, requester: %d\n", *myAddress,getID(requester));
+    LOG("~~~~> procurando pacote no pipe!! eu: %x, requester: %d\n", *myAddress,getID(requester));
     unsigned int found = PIPE_WAIT;
     //unsigned int foundHist = PIPE_WAIT;
     unsigned int foundSent = PIPE_WAIT;
@@ -781,7 +781,7 @@ unsigned int sendFromMsgBuffer(unsigned int requester){
         return 1; // sent with success
     }
     else{
-        //LOG("~~~~> NAO! ENCONTRADO!! eu: %x, requester: %d\n", *myAddress,getID(requester));   
+        LOG("~~~~> NAO! ENCONTRADO!! eu: %x, requester: %d\n", *myAddress,getID(requester));   
         return 0; // packet is not in the buffer yet
     }
 }
