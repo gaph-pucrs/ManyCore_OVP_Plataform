1f3f1f109079ae06dd6176d8719497e5c874e1a7
iacanaw <iacanaw@gmail.com>
2020-06-16 15:03:14 -0300

    Update harness.c

--- harness/harness.c
+++ harness/harness.c
@@ -18,9 +18,9 @@
      ((((x) & 0xff000000) >> 24) | (((x) & 0x00ff0000) >>  8) |		      \
       (((x) & 0x0000ff00) <<  8) | (((x) & 0x000000ff) << 24))
 /* Quantum defines */
-#define INSTRUCTIONS_PER_SECOND       1000000000 // 1GHz (assuming 1 instruction per cycle)
-#define QUANTUM_TIME_SLICE            0.00001    // 10us quantum time step       
-#define INSTRUCTIONS_PER_TIME_SLICE   (INSTRUCTIONS_PER_SECOND*QUANTUM_TIME_SLICE)  // 10000 instructions per quantum
+#define INSTRUCTIONS_PER_SECOND       250000000 //250MHz -------- //1000000000 // 1GHz (assuming 1 instruction per cycle)
+#define QUANTUM_TIME_SLICE            0.00001   // 10us quantum time step       
+#define INSTRUCTIONS_PER_TIME_SLICE   (INSTRUCTIONS_PER_SECOND*QUANTUM_TIME_SLICE)  // 2500 //10000 instructions per quantum
 
 #define BRANCH  1
 #define ARITH   2
