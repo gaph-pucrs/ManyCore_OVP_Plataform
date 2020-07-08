ff3a6f5201c65daebf629d6237197513aca73327
iacanaw <iacanaw@gmail.com>
2020-07-08 15:33:11 -0300

    new app synthetic

---
+++ application/source/synthetic/taskE.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "interrupt.h"
+#include "spr_defs.h"
+#include "source/API/api.h"
+
+#include "synthetic_config.h"
+
+message theMessage;
+
+int main(int argc, char **argv)
+{
+    OVP_init();
+    //////////////////////////////////////////////////////
+    /////////////// YOUR CODE START HERE /////////////////
+    //////////////////////////////////////////////////////
+    int i, j, t;
+	
+	LOG("synthetic task E started.\n");
+
+	for(i=0;i<SYNTHETIC_ITERATIONS;i++){
+	
+		theMessage.size = 30;
+		for(j=0;j<30;j++) theMessage.msg[j]=i;
+
+		ReceiveMessage(&theMessage, taskC_addr);
+
+		for(t=0;t<1000;t++){
+		}
+
+		SendMessage(&theMessage, taskF_addr);
+		
+		LOG("taskE - %d\n",i);
+	}
+
+    LOG("synthetic task E finished.\n");
+	//////////////////////////////////////////////////////
+    //////////////// YOUR CODE ENDS HERE /////////////////
+    //////////////////////////////////////////////////////
+    FinishApplication();
+    return 1;
+}
