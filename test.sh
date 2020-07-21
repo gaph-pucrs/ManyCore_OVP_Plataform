e51f3902381849ef85c7c67abd3603ec223b3082
iacanaw <iacanaw@gmail.com>
2020-07-21 15:59:56 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-cp -f /application/source/dijkstra/scenario.yaml /application
+cp -f application/source/dijkstra/scenario.yaml application/
 ./run.sh 4 4
-mv /simulation/power_log.txt /simulation/power_log_dijkstra_250M.txt
-cp -f /application/source/synthetic/scenario.yaml /application
+mv simulation/power_log.txt simulation/power_log_dijkstra_1G.txt
+cp -f application/source/synthetic/scenario.yaml application/
 ./run.sh 4 4
-mv /simulation/power_log.txt /simulation/power_log_synthetic_250M.txt
+mv simulation/power_log.txt simulation/power_log_synthetic_1G.txt
