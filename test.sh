301a8ff759d2ad2c91db76450018349e717a0110
iacanaw <iacanaw@gmail.com>
2020-07-21 16:33:35 -0300

    250mhz

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 cp -f application/source/dijkstra/scenario.yaml application/
 ./run.sh 4 4
-mv -f simulation/power_log.txt simulation/saved/power_log_dijkstra_1G.txt
+mv -f simulation/power_log.txt simulation/saved/power_log_dijkstra_250M.txt
 cp -f application/source/synthetic/scenario.yaml application/
 ./run.sh 4 4
-mv -f simulation/power_log.txt simulation/saved/power_log_synthetic_1G.txt
+mv -f simulation/power_log.txt simulation/saved/power_log_synthetic_250M.txt
