524552f0fdbc2044264486392a9e5c3e5eef34be
Iaçanã Weber <iacanaw@gmail.com>
2020-08-25 07:30:51 -0300

    Update generateFlowGraph.py

--- scripts/generateFlowGraph.py
+++ scripts/generateFlowGraph.py
@@ -26,6 +26,34 @@ def getMaxValue(local,east,west,north,south,max):
         max = south
     return max
 
+def printToGraph(id, graph, quantunsPerGraph, local, east, west, north, south):
+    myY = id/DIM_X
+    myX = id-(DIM_X*myY)
+    centralX = (myX*3) + 1
+    centralY = (myY*3) + 1
+    with open('/myGraphs/graph'+str(graph)+'.dat',"a") as gfile:
+        # canto esquerdo inferior (nada)
+        print(str((centralX-1))+" "+str((centralY-1))+" "+str((graph+1)*quantunsPerGraph)+" "+str(0), file=gfile)
+        # meio inferior (south)
+        print(str((centralX))+" "+str((centralY-1))+" "+str((graph+1)*quantunsPerGraph)+" "+str(south), file=gfile)
+        # canto direito inferior (nada)
+        print(str((centralX+1))+" "+str((centralY-1))+" "+str((graph+1)*quantunsPerGraph)+" "+str(0), file=gfile)
+        
+        # borda esquerda (west)
+        print(str((centralX-1))+" "+str((centralY))+" "+str((graph+1)*quantunsPerGraph)+" "+str(west), file=gfile)
+        # central (local)
+        print(str((centralX))+" "+str((centralY))+" "+str((graph+1)*quantunsPerGraph)+" "+str(local), file=gfile)
+        # borda direita (east)
+        print(str((centralX+1))+" "+str((centralY))+" "+str((graph+1)*quantunsPerGraph)+" "+str(east), file=gfile)
+        
+        # canto esquerdo superior (nada)
+        print(str((centralX-1))+" "+str((centralY+1))+" "+str((graph+1)*quantunsPerGraph)+" "+str(0), file=gfile)
+        # meio superior (north)
+        print(str((centralX))+" "+str((centralY+1))+" "+str((graph+1)*quantunsPerGraph)+" "+str(north), file=gfile)
+        # canto direito superior (nada)
+        print(str((centralX+1))+" "+str((centralY+1))+" "+str((graph+1)*quantunsPerGraph)+" "+str(0), file=gfile)
+        
+
 if __name__ == '__main__':
 
     with open('../simulation/flitsLog.txt') as csv_file:
@@ -46,7 +74,8 @@ if __name__ == '__main__':
                     southFlow[i] += int(theLine[6])
             for i in range(N_PES):
                 maxValue = getMaxValue(localFlow[i], eastFlow[i], westFlow[i], northFlow[i], southFlow[i], maxValue)
-                print(str(i)+" "+str((graph+1)*quantunsPerGraph)+" "+str(localFlow[i])+" "+str(eastFlow[i])+" "+str(westFlow[i])+" "+str(northFlow[i])+" "+str(southFlow[i]))
+                printToGraph(i, graph, quantunsPerGraph, localFlow[i], eastFlow[i], westFlow[i], northFlow[i], southFlow[i])
+                #print(str(i)+" "+str((graph+1)*quantunsPerGraph)+" "+str(localFlow[i])+" "+str(eastFlow[i])+" "+str(westFlow[i])+" "+str(northFlow[i])+" "+str(southFlow[i]))
                 localFlow[i] = 0
                 eastFlow[i]  = 0
                 westFlow[i]  = 0
