feb0e592f82aa83ae8aa3d108649123a659a2405
Iaçanã Weber <iacanaw@gmail.com>
2020-08-25 05:30:01 -0300

    Create generateFlowGraph.py

---
+++ scripts/generateFlowGraph.py
@@ -0,0 +1,14 @@
+import csv
+import numpy as np
+
+DIM_X = 4
+DIM_Y = 4
+N_PES = DIM_X*DIM_Y
+
+if __name__ == '__main__':
+
+    with open('../simulation/flitsLog.txt') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        data = list(csv_reader)
+    
+    print(data)
