7d653d7de413525c6055db4c907932b75c4efcf2
Iaçanã Weber <iacanaw@gmail.com>
2020-09-28 02:38:56 -0300

    Update chronos.py

--- chronos.py
+++ chronos.py
@@ -10,16 +10,12 @@ import yaml
 
 if __name__ == '__main__':
     # Read Arguments    
-    try:
-        TESTCASE_FILE = sys.argv[1]
-        print(TESTCASE_FILE)
-        with open('sandbox/'+str(TESTCASE_FILE)+'.yaml') as file:
-	        scenario = yaml.load(file, Loader=yaml.SafeLoader)
-            DIMENSIONS = scenario['hw'][0]['mpsoc_dimension']
-            X = DIMENSIONS[0]
-            Y = DIMENSIONS[1]
-            print("RUNNING X="+str(X)+"  Y="+str(Y))
-    except:
-        print("ERROR1: You need to expecify a valid yaml TESTCASE_FILE")
+    TESTCASE_FILE = sys.argv[1]
+    with open('sandbox/'+str(TESTCASE_FILE)+'.yaml') as file:
+        testcase = yaml.load(file, Loader=yaml.SafeLoader)
+        DIMENSIONS = testcase['hw'][0]['mpsoc_dimension']
+        X = DIMENSIONS[0]
+        Y = DIMENSIONS[1]
+        print("RUNNING X="+str(X)+"  Y="+str(Y))
 
 
