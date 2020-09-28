4879eb33d1bbbb72ce8acdccc6e07c2ba7cf7638
Iaçanã Weber <iacanaw@gmail.com>
2020-09-28 02:30:55 -0300

    Update chronos.py

--- chronos.py
+++ chronos.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
         TESTCASE_FILE = sys.argv[1]
         with open('sandbox/'+str(TESTCASE_FILE)+'.yaml') as file:
 	        scenario = yaml.load(file, Loader=yaml.SafeLoader)
-            DIMENSIONS = scenario['hw']['mpsoc_dimension']
+            DIMENSIONS = scenario['hw'][0]['mpsoc_dimension']
             X = DIMENSIONS[0]
             Y = DIMENSIONS[1]
             print("RUNNING X="+str(X)+"  Y="+str(Y))
