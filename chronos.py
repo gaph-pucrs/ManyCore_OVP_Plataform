6f3705b9b2f703c5f072bc3ea8bdb8dcda1ed9ab
Iaçanã Weber <iacanaw@gmail.com>
2020-09-28 02:24:36 -0300

    CHRONOS ADDED

---
+++ chronos.py
@@ -0,0 +1,16 @@
+import sys
+import yaml
+
+if __name__ == '__main__':
+    # Read Arguments    
+    try:
+        TESTCASE_FILE = sys.argv[1]
+        with open('sandbox/'+str(TESTCASE_FILE)+'.yaml') as file:
+	        scenario = yaml.load(file, Loader=yaml.SafeLoader)
+            X = int(scenario['hw']['mpsoc_dimension'][0])
+            Y = int(scenario['hw']['mpsoc_dimension'][1])
+            print("RUNNING X="+str(X)+"  Y="+str(Y))
+    except:
+        print("ERROR1: You need to expecify a valid yaml TESTCASE_FILE")
+        
+
