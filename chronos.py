bf2f9753d4fd47a41b425d8ddfe0de24a93725b5
Iaçanã Weber <iacanaw@gmail.com>
2020-09-28 02:50:46 -0300

    Update chronos.py

--- chronos.py
+++ chronos.py
@@ -11,12 +11,13 @@ import yaml
 if __name__ == '__main__':
     # Read Arguments    
     TESTCASE_FILE = sys.argv[1]
-    print(TESTCASE_FILE)
-    with open('sandbox/'+str(TESTCASE_FILE)+'.yaml') as file:
-        testcase = yaml.load(file, Loader=yaml.SafeLoader)
-        DIMENSIONS = testcase['hw']['mpsoc_dimension']
-        X = DIMENSIONS[0]
-        Y = DIMENSIONS[1]
-        print("RUNNING X="+str(X)+"  Y="+str(Y))
-
+    try:
+        with open('sandbox/'+str(TESTCASE_FILE)+'.yaml') as file:
+            testcase = yaml.load(file, Loader=yaml.SafeLoader)
+            DIMENSIONS = testcase['hw']['mpsoc_dimension']
+            X = DIMENSIONS[0]
+            Y = DIMENSIONS[1]
+            print("RUNNING X="+str(X)+"  Y="+str(Y))
+    except:
+        print("ERRO1: You need to call a valid TESTCASE")
 
