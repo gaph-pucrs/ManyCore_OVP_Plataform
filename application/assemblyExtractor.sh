3aff7f711dd65341a550d0a0bbc661de6f31a1a2
Iaçanã Weber <iacanaw@gmail.com>
2020-09-29 04:51:18 -0300

    number

--- application/assemblyExtractor.sh
+++ application/assemblyExtractor.sh
@@ -2,5 +2,6 @@
 
 X=$1
 SCENARIO_FILE=$2
+APP_N=$3
 
-/soft64/imperas/ferramentas/Imperas.20191106/lib/Linux64/CrossCompiler/or32-elf/bin/or32-elf-objdump -d "$X" >> ../sandbox/$SCENARIO_FILE/applications/assembly/$X.S
+/soft64/imperas/ferramentas/Imperas.20191106/lib/Linux64/CrossCompiler/or32-elf/bin/or32-elf-objdump -d "$X" >> ../sandbox/$SCENARIO_FILE/applications/assembly/application"$APP_N".S
