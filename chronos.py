28490c61129d7847ef546e50dca31d8771e39f28
Iaçanã Weber <iacanaw@gmail.com>
2020-09-29 03:23:14 -0300

    removing old stuff

--- chronos.py
+++ chronos.py
@@ -48,10 +48,13 @@ if __name__ == '__main__':
         sys.exit(1)
     # VERIFY THE SCENARIO FILE
     if(os.path.isfile('sandbox/'+str(SCENARIO_FILE)+'.yaml')):
+        # CALL THE RUN SCRIPT TO START THE SIMULATION
         subprocess.call("./run.sh "+str(X)+" "+str(Y)+" "+SCENARIO_FILE, shell=True)
     else:
         print("ERRO 2: You need to call a valid SCENARIO.")
         sys.exit(1)
+
+
 
 
     #COMANDO PARA ABRIR O OVP EM OUTRA JANELA
