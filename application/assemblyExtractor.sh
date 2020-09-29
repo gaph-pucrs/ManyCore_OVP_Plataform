df7b6db497570ba6cd23e8a5b78ceb346d050af4
Iaçanã Weber <iacanaw@gmail.com>
2020-09-29 04:48:22 -0300

    Update assemblyExtractor.sh

--- application/assemblyExtractor.sh
+++ application/assemblyExtractor.sh
@@ -3,4 +3,4 @@
 X=$1
 SCENARIO_FILE=$2
 
-/soft64/imperas/ferramentas/Imperas.20191106/lib/Linux64/CrossCompiler/or32-elf/bin/or32-elf-objdump -d "$X" > ../sandbox/$SCENARIO_FILE/applications/assembly/$X.S
+/soft64/imperas/ferramentas/Imperas.20191106/lib/Linux64/CrossCompiler/or32-elf/bin/or32-elf-objdump -d "$X" #> ../sandbox/$SCENARIO_FILE/applications/assembly/$X.S
