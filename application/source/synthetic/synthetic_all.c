97c1519c3daa06281bca2a1703902bcee2f6a6ee
Iaçanã Weber <iacanaw@gmail.com>
2020-10-08 05:46:47 -0300

    Update synthetic_all.c

--- application/source/synthetic/synthetic_all.c
+++ application/source/synthetic/synthetic_all.c
@@ -10,9 +10,8 @@ int synthetic_taskA(int state){
 		theMessage.size = 30;
 		for(j=0;j<30;j++) theMessage.msg[j]=i;
 		
-		printi("enviando...\n");
+		
 		SendMessage(&theMessage, taskC);
-		printi("enviado!\n");
 		printi(clock()); prints("taskA\n");
 		printi(clock()); printi(i); prints("\n");
 
