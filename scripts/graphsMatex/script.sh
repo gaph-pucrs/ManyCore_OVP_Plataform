99b15e413a5a1a1b6159f793bdf9da15b0dc9a57
Iaçanã Weber <iacanaw@gmail.com>
2020-10-09 13:47:09 -0300

    Update script.sh

--- scripts/graphsMatex/script.sh
+++ scripts/graphsMatex/script.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo $1
-cat ../simulation/matex.txt | grep Temperatures > Temp
-cat ../simulation/matex.txt | grep Power: > Power
-cat ../simulation/matex.txt | grep Power_int: > Power_int
+cat ../../simulation/matex.txt | grep Temperatures > Temp
+cat ../../simulation/matex.txt | grep Power: > Power
+cat ../../simulation/matex.txt | grep Power_int: > Power_int
 
 python temp_offset.py Temp Temperatures
 python power_total.py Power PowerTotal
