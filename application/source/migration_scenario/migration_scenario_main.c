39c9cac4c7e4a42b1b214e63cb00cc3c8f714c4f
Iaçanã Weber <iacanaw@gmail.com>
2020-10-17 04:18:29 -0300

    reverting sort

--- application/source/migration_scenario/migration_scenario_main.c
+++ application/source/migration_scenario/migration_scenario_main.c
@@ -8,7 +8,7 @@
 
 #include "synthetic_config.h"
 #include "dijkstra_config.h"
-#include "sort_config.h"
+//#include "sort_config.h"
 //#include "aes_config.h"
 #include "mpeg_config.h"
 #include "dtw_config.h"
@@ -172,19 +172,19 @@ int main(int argc, char **argv)
 			case start:
 				state = mpeg_start(state);
 				break;
-			//Sort
-			case sort_master:
-				state = sortMaster(state);
-				break;
-			case sort_slave1:
-				state = sort_slave(0);
-				break;
-			case sort_slave2:
-				state = sort_slave(1);
-				break;
-			case sort_slave3:
-				state = sort_slave(2);
-				break;
+			// //Sort
+			// case sort_master:
+			// 	state = sortMaster(state);
+			// 	break;
+			// case sort_slave1:
+			// 	state = sort_slave(0);
+			// 	break;
+			// case sort_slave2:
+			// 	state = sort_slave(1);
+			// 	break;
+			// case sort_slave3:
+			// 	state = sort_slave(2);
+			// 	break;
 			// //AES
 			// case aes_master:
 			// 	state = aesMaster(state);
