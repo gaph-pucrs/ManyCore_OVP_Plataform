81f8be0ec0036b06a809d6f92e7da7f5ed3c2f8d
Iaçanã Weber <iacanaw@gmail.com>
2020-10-20 01:36:42 -0300

    Update api.h

--- application/source/API/api.h
+++ application/source/API/api.h
@@ -1052,12 +1052,12 @@ void SendRaw(unsigned int addr){
 void FinishApplication(){
     unsigned int done;
     unsigned int i;
-    do{
+    /*do{
         done = 1; // assumes that every packet was transmitted 
         for(i=0;i<PIPE_SIZE;i++){
             if(buffer_map[i]!=EMPTY) done = 0; // if some position in the buffer is occupied then the program must wait!
         }
-    }while(done==0);
+    }while(done==0);*/
 
     // Activate the clock gating and waits until every other processor finish its task
 #if USE_THERMAL
