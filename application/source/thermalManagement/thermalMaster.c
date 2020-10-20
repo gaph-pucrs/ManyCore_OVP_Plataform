528d0e26e98102080aa2e2c9db49e49f0d5c277f
Iaçanã Weber <iacanaw@gmail.com>
2020-10-20 11:03:57 -0300

    testing av

--- application/source/thermalManagement/thermalMaster.c
+++ application/source/thermalManagement/thermalMaster.c
@@ -379,7 +379,6 @@ int main(int argc, char **argv)
                 // putsv("energy ", energy_i[i]);
                 // putsv("control_signal ", control_signal[i]);
             }
-            //generateTempMatrix(Temperature); //
             generateTempMatrix(control_signal);
 
             if ((time+1)%20 == 0){
