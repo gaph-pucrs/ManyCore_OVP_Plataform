15d3e78d11023ae7dba2b60100a3f9f5fb649eb8
Iaçanã Weber <iacanaw@gmail.com>
2020-10-25 05:04:28 -0300

    Update thermalMaster.c

--- application/source/thermalManagement/thermalMaster.c
+++ application/source/thermalManagement/thermalMaster.c
@@ -345,7 +345,8 @@ int main(int argc, char **argv)
             task_name[strlen(task_name)-1] = '\0';
             task_number = strtok(NULL, " ");
             task_number[strlen(task_number)-1] = '\0';
-            task_addr[tasks_to_map] = 0;
+            task_current_addr[tasks_to_map] = 0xFFFFFFFF;
+            task_future_addr[task_to_map] = 0xFFFFFFFF;
             task_start_time[tasks_to_map] = starting_time;
             task_remaining_executions[tasks_to_map] = executions;
             task_repeat_after[tasks_to_map] = repeat_after;
