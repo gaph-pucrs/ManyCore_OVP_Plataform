1d3b890089352330239f9747fef512b3defd707b
iacanaw <iacanaw@gmail.com>
2020-10-27 02:50:59 -0300

    not working

--- application/application0.c
+++ application/application0.c
@@ -122,8 +122,8 @@ int migrationEnvolved(unsigned int pe, unsigned int task_confirmed_addr[DIM_X*DI
     putsv("pe = ", pe);
     for(i = 0; i < DIM_X*DIM_Y; i++){
         putsvsv("regular[", i,"] = ", task_addr[i]);
-        putsvsv("regular[", i,"] = ", task_confirmed_addr[i]);
-        prints("---------");
+        putsvsv("confirm[", i,"] = ", task_confirmed_addr[i]);
+        prints("---------\n");
         if(task_addr[i] == pe && foundI == -1){
             foundI = i;
         }
@@ -135,22 +135,22 @@ int migrationEnvolved(unsigned int pe, unsigned int task_confirmed_addr[DIM_X*DI
                 return FALSE;
             }
             else{
-                if(foundI == -1 && foundJ == -1){
-                    return FALSE;
-                }
                 return TRUE;
             }
         }
     }
-
-    /*for(i = 0; i < DIM_X*DIM_Y; i++){
-        if(task_addr[i] == pe){ // found a task that is running this PE
-            if(task_confirmed_addr[i] == pe) // if the occupation is confirmed then
-                return FALSE; // this PE is not envolved in any migration
-            else
-                return TRUE;
-        }
-    }*/
+    if(foundI == -1 && foundJ == -1){
+        return FALSE;
+    }
+    else if(foundI != -1 && foundJ == -1){
+        return TRUE;
+    }
+    else if(foundI == -1 && foundJ != -1){
+        return TRUE;
+    }
+    else{
+        while(1){LOG("EROOOOOOOOUUU! 55555555555555555555\n");}
+    }
 }
 
 int temperature_migration(unsigned int temp[DIM_X*DIM_Y], unsigned int tasks_to_map, unsigned int task_addr[DIM_X*DIM_Y]){
