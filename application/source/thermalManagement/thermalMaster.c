53a6e72d348ecca93d3f4fbefc7f393d257b4c30
Iaçanã Weber <iacanaw@gmail.com>
2020-10-28 07:16:54 -0300

    testing Synth

--- application/source/thermalManagement/thermalMaster.c
+++ application/source/thermalManagement/thermalMaster.c
@@ -409,7 +409,7 @@ int main(int argc, char **argv)
             aux_str = strtok(line, ":");
             aux_str = strtok(NULL, " ");
             aux_str[strlen(app_name)-1] = '\0';
-            executions = atoi(aux_str);
+            executions = atoi(aux_str) + 1;
         }        
     }
     
