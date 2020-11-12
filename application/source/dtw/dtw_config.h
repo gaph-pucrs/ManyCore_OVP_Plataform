bc48fa50dcba567c3b0885d194207b2e9b18e514
Iaçanã Weber <iacanaw@gmail.com>
2020-11-12 05:45:02 -0300

    7

--- application/source/dtw/dtw_config.h
+++ application/source/dtw/dtw_config.h
@@ -1,5 +1,4 @@
-#define MATX_SIZE 			11		  	//tamanho da matriz
-#define TOTAL_TASKS 		4 			//deve ser PAR para dividir igualmente o numero de padroes por task
-#define NUM_PATTERNS 		12000 		//40
-#define PATTERN_PER_TASK 	NUM_PATTERNS/TOTAL_TASKS
-
+#define MATX_SIZE 11       //tamanho da matriz
+#define TOTAL_TASKS 4      //deve ser PAR para dividir igualmente o numero de padroes por task
+#define NUM_PATTERNS 1200  //40
+#define PATTERN_PER_TASK NUM_PATTERNS / TOTAL_TASKS
