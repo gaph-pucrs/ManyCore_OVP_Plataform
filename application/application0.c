99c6cc2ea8dbd6c338e86d365f33ca22cb66dd82
iacanaw <iacanaw@gmail.com>
2020-11-12 21:06:15 -0300

    not working

--- application/application0.c
+++ application/application0.c
@@ -337,7 +337,7 @@ int getSpiralMatixEmptyPE(unsigned int task_addr[DIM_X * DIM_Y], int appID) {
     int i, j, k, empty, pe;
 
     for (k = 0; k < DIM_Y * DIM_Y; k += (QUAD_DIM_X * QUAD_DIM_Y)) {
-        if (appQuadrant[appID] == -1 && k == 0) {
+        if (appQuadrant[appID] == -1) {
             appQuadrant[appID] = getCoolestQuad();
         }
 
@@ -635,6 +635,7 @@ int main(int argc, char **argv) {
                     //finishedTasks++;
                     task_start_time[i] = measuredWindows + task_repeat_after[i];
                     finishedTask[i] = 2;
+                    appQuadrant[task_applicationID[i]] = -1;  // reset the app quadrant to get a new one if the app restarts
                     putsvsv("Task ", i, " restarting at (ms) ", task_start_time[i]);
                 } else if (finishedTask[i] == FALSE) {
                     finishSimulation = 0;
