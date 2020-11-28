b3eb3f0c4fdc5367aa43d3c05bb8533f1690993b
Iaçanã Weber <iacanaw@gmail.com>
2020-11-28 03:11:16 -0300

    dijkstra update

--- application/source/dijkstra/print_dij.c
+++ application/source/dijkstra/print_dij.c
@@ -1,46 +1,45 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include "interrupt.h"
-#include "spr_defs.h"
 #include "source/API/api.h"
+#include "spr_defs.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "dijkstra_config.h"
 
 message theMessage;
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     OVP_init();
-	prints("STARTING PRINT\n"); 
+    prints("STARTING PRINT\n");
     //////////////////////////////////////////////////////
     /////////////// YOUR CODE START HERE /////////////////
     //////////////////////////////////////////////////////
-	int k;
-	theMessage.size = NUM_NODES*2;
-	int result[NUM_NODES*2];
+    int k;
+    theMessage.size = NUM_NODES * 2;
+    int result[NUM_NODES * 2];
 
-	ReceiveMessage(&theMessage, dijkstra_0_addr);
-	for (k=0; k < NUM_NODES*2; k++)
-		result[k] = theMessage.msg[k];
+    ReceiveMessage(&theMessage, dijkstra_0);
+    for (k = 0; k < NUM_NODES * 2; k++)
+        result[k] = theMessage.msg[k];
 
-	ReceiveMessage(&theMessage, dijkstra_1_addr);
-	for (k=0; k < NUM_NODES*2; k++)
-		result[k] = theMessage.msg[k];
+    ReceiveMessage(&theMessage, dijkstra_1);
+    for (k = 0; k < NUM_NODES * 2; k++)
+        result[k] = theMessage.msg[k];
 
-	ReceiveMessage(&theMessage, dijkstra_2_addr);
-	for (k=0; k < NUM_NODES*2; k++)
-		result[k] = theMessage.msg[k];
+    ReceiveMessage(&theMessage, dijkstra_2);
+    for (k = 0; k < NUM_NODES * 2; k++)
+        result[k] = theMessage.msg[k];
 
-	ReceiveMessage(&theMessage, dijkstra_3_addr);
-	for (k=0; k < NUM_NODES*2; k++)
-		result[k] = theMessage.msg[k];
+    ReceiveMessage(&theMessage, dijkstra_3);
+    for (k = 0; k < NUM_NODES * 2; k++)
+        result[k] = theMessage.msg[k];
 
-	ReceiveMessage(&theMessage, dijkstra_4_addr);
-	for (k=0; k < NUM_NODES*2; k++)
-		result[k] = theMessage.msg[k];
+    ReceiveMessage(&theMessage, dijkstra_4);
+    for (k = 0; k < NUM_NODES * 2; k++)
+        result[k] = theMessage.msg[k];
 
-	prints("PRINT FINISHED\n");
+    prints("PRINT FINISHED\n");
     //////////////////////////////////////////////////////
     //////////////// YOUR CODE ENDS HERE /////////////////
     //////////////////////////////////////////////////////
